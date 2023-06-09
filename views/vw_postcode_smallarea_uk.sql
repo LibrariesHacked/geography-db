95bef79a68bd67d2a28f12fa9dbdbafc47bfc2e7
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-06-09 07:05:51 +0100

    Fixing postcode load

--- views/vw_postcode_smallarea_uk.sql
+++ views/vw_postcode_smallarea_uk.sql
@@ -2,6 +2,6 @@ create view vw_postcode_smallarea_uk as
 select
     p.postcode,
     p.postcode_sector_trimmed,
-    case when p.country = 'N92000002' then p.oa else p.lsoa end as smallarea,
+    case when p.country = 'N92000002' then p.oa_21 else p.lsoa_21 end as smallarea,
 	p.terminated
 from postcode_lookup p;
